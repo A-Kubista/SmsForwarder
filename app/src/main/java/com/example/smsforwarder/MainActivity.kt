4f2b9397197f268f78f884320fdb7afee5b277a3
A-Kubista <aleksanderkubista@gmail.com>
2022-04-15 23:36:10 +0200

    fix

--- app/src/main/java/com/example/smsforwarder/MainActivity.kt
+++ app/src/main/java/com/example/smsforwarder/MainActivity.kt
@@ -1,32 +1,57 @@
 package com.example.smsforwarder
 
 import android.Manifest
+import android.Manifest.permission.RECEIVE_SMS
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.annotation.NonNull
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
 
-
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestPermissions(
-            arrayOf(
-                Manifest.permission.SEND_SMS
-            ), 0
-        )
+
+        findViewById<Button>(R.id.permissions).setOnClickListener{
+                    requestPermissions(  arrayOf(
+                        Manifest.permission.RECEIVE_SMS,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.SEND_SMS
+                    ), 1 )
+
+
+        }
+
         val sharedPreferences =
             getSharedPreferences(getString(R.string.app_shared_preferences_key), MODE_PRIVATE)
         initPhoneNumberInput(sharedPreferences)
+
+        val lastMsg = findViewById<View>(R.id.lastMsg) as TextView
+        lastMsg.text = sharedPreferences.getString(
+            "lastMessage",
+            "no message in cache"
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "pr " + requestCode + "  result ture ", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun initPhoneNumberInput(sharedPreferences: SharedPreferences) {
@@ -34,7 +59,7 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<View>(R.id.targetPhoneNumberInput) as EditText
         editText.setText(
             sharedPreferences.getString(
-                "lastMessage",
+                "number",
                 getString(R.string.default_forward_number)
             )
         )
--- app/src/main/java/com/example/smsforwarder/SMSReceiver.kt
+++ app/src/main/java/com/example/smsforwarder/SMSReceiver.kt
@@ -1,80 +1,103 @@
-package com.example.smsforwarder;
+package com.example.smsforwarder
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
-import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.telephony.SmsMessage;
+import android.content.BroadcastReceiver
+import android.content.Context
+import androidx.annotation.RequiresApi
+import android.os.Build
+import android.content.Intent
+import com.example.smsforwarder.SMSReceiver
+import android.os.Bundle
+import android.content.SharedPreferences
+import android.provider.Telephony
+import android.telephony.SmsManager
+import android.telephony.SmsMessage
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.smsforwarder.R
+import java.nio.charset.Charset
 
-import androidx.annotation.RequiresApi;
+class SMSReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action != SMS_RECEIVED_ACTION) return
+        val bundle = intent.extras
+        val pduObjects = bundle!!["pdus"] as Array<Any>? ?: return
+        val sharedPreferences =
+            context.getSharedPreferences(context.getString(R.string.app_shared_preferences_key), Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val targetNumber = sharedPreferences.getString(
+            "number",
+            context.getString(R.string.default_forward_number)
+        )
+        for (messageObj in pduObjects) {
 
-public class SMSReceiver extends BroadcastReceiver {
-    static final String SMS_RECEIVED_ACTION = android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION;
-    static final SmsManager smsManager = SmsManager.getDefault();
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (! intent.getAction().equals(SMS_RECEIVED_ACTION)) return;
-
-        final Bundle bundle = intent.getExtras();
-        final Object[] pduObjects = (Object[]) bundle.get("pdus");
-        if (pduObjects == null) return;
-
-        final SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.app_shared_preferences_key), Context.MODE_PRIVATE);
-        final String targetNumber = sharedPreferences.getString(context.getString(R.string.target_phone_number_key), context.getString(R.string.default_forward_number));
-
-        for (Object messageObj : pduObjects) {
-            SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) messageObj, (String) bundle.get("format"));
-
-           SharedPreferences.Editor editor =  sharedPreferences.edit();
-            editor.putString("lastMessage", "lst msg " + currentMessage.getMessageBody()).apply();
-
-            String senderNumber = currentMessage.getDisplayOriginatingAddress();
-            String rawMessageContent = currentMessage.getDisplayMessageBody();
+            val currentMessage = SmsMessage.createFromPdu(messageObj as ByteArray, bundle["format"] as String?)
+            editor.putString("lastMessage", "lst msg " + currentMessage.messageBody).apply()
+            val senderNumber = currentMessage.displayOriginatingAddress
+            val rawMessageContent = currentMessage.displayMessageBody
 
             /**
              * when receive a message from ordinary numbers
              * forward the message to target number in this format:
-             *      'from `senderNumber` :`messageContent`'
+             * 'from `senderNumber` :`messageContent`'
              *
              * when receive a message from the target number
-             *      e.g. target phone replying the message
+             * e.g. target phone replying the message
              * the format should be:
-             *      'to `toNumber` :`messageContent`'
+             * 'to `toNumber` :`messageContent`'
              * then forward the message to 'toNumber'
              */
-            String forwardNumber = null;
-            String forwardPrefix = null;
-            String forwardContent = null;
-            if (senderNumber.matches(targetNumber)) {
-                // it's a message from target number
-                String formatRegex = "to (\\+?\\d+?):\\n((.|\\n)*)";
-                if (rawMessageContent.matches(formatRegex)) {
-                    // it'a message need to be forwarded
-                    forwardNumber = rawMessageContent.replaceFirst(formatRegex, "$1");
-                    forwardPrefix = "";
-                    forwardContent = rawMessageContent.replaceFirst(formatRegex, "$2");
-                }
+            var forwardNumber: String? = null
+            var forwardPrefix: String? = null
+            var forwardContent: String? = null
+            if (senderNumber.equals(targetNumber)) {
+                postReceivedSms(rawMessageContent,context)
             } else {
                 // it's a normal message, need to be forwarded
-                forwardNumber = targetNumber;
-                forwardPrefix = "from " + senderNumber + ":\n";
-                forwardContent = rawMessageContent;
+                forwardNumber = targetNumber
+                forwardPrefix = "from $senderNumber:\n"
+                forwardContent = rawMessageContent
             }
-
             if (forwardNumber != null && forwardContent != null) {
-                if ((forwardPrefix + forwardContent).getBytes().length > 120) {
+                if ((forwardPrefix + forwardContent).toByteArray().size > 120) {
                     // there is a length limit in SMS, if the message length exceeds it, separate the meta data and content
-                    smsManager.sendTextMessage(forwardNumber, null, forwardPrefix, null, null);
-                    smsManager.sendTextMessage(forwardNumber, null, forwardContent, null, null);
+                    smsManager.sendTextMessage(forwardNumber, null, forwardPrefix, null, null)
+                    smsManager.sendTextMessage(forwardNumber, null, forwardContent, null, null)
                 } else {
                     // if it's not too long, combine meta data and content to save money
-                    smsManager.sendTextMessage(forwardNumber, null, forwardPrefix + forwardContent, null, null);
+                    smsManager.sendTextMessage(forwardNumber, null, forwardPrefix + forwardContent, null, null)
                 }
             }
         }
+    }
+
+    fun postReceivedSms(msg : String, context :  Context) {
+        val queue = Volley.newRequestQueue(context)
+        val url = "https://github.com/ecoenergysoultions/sms-forwarder/blob/master/forwarder/handler.py"
+
+        val requestBody = "&msg=${msg}"
+        val stringReq : StringRequest =
+            object : StringRequest(Method.POST, url,
+                Response.Listener { response ->
+                    // response
+                    var strResp = response.toString()
+                    Log.d("API", strResp)
+                },
+                Response.ErrorListener { error ->
+                    Log.d("API", "error => $error")
+                }
+            ){
+                override fun getBody(): ByteArray {
+                    return requestBody.toByteArray(Charset.defaultCharset())
+                }
+            }
+        queue.add(stringReq)
+    }
+
+    companion object {
+        const val SMS_RECEIVED_ACTION = Telephony.Sms.Intents.SMS_RECEIVED_ACTION
+        val smsManager = SmsManager.getDefault()
     }
 }
